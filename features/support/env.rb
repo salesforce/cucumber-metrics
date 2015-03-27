0404e71e152380dcf5e6a233c3915e7d1641a277
eric@deskcom <eric@desk.com>
2015-03-27 16:41:20 -0400

    added comments in env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,5 +1,10 @@
 require 'mysql2'
 
+# BROWSER and TESTENV are required for the gem to work correctly, but
+# can be named whatever you want. The values you assign here will be
+# saved in the browsers and test_environments tables, respectively. If
+# you don't use a browser or environment for your tests, just assign a
+# placeholder string to these constants (or leave them as they are).
 BROWSER = "firefox"
-TESTENV = "localhost:3000"
+TESTENV = "staging"
 METRICS_CONFIG_FILE = File.expand_path("config/metrics.yml")
